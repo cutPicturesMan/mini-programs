b16476dd1720d698126cf6235c505fd576c63ac8
臻 <408869403@qq.com>
2017-09-23 09:59:19 +0800

    修改部分样式

--- app.js
+++ app.js
@@ -5,23 +5,21 @@ App({
     // 全局的sessionId
     sessionId: wx.getStorageSync('sessionId') || null
   },
-
+  //
   // onLaunch () {
-    // // 检查登录状态
-    // wx.checkSession({
-    //   success: () => {
-    //     console.log('checkSession 成功');
-    //     this.logs(new Date() + '登录成功');
-    //   },
-    //   fail: () => {
-    //     console.log('checkSession 失败');
-    //     // 如果失败，重新登录
-    //     this.login();
-    //   }
-    // });
+  //   // 检查登录状态
+  //   wx.checkSession({
+  //     success: () => {
+  //       console.log('checkSession 成功');
+  //       this.logs(new Date() + '登录成功');
+  //     },
+  //     fail: () => {
+  //       console.log('checkSession 失败');
+  //       // 如果失败，重新登录
+  //       this.login();
+  //     }
+  //   });
   // },
-
-
 
   getUserInfo: function (cb) {
     var that = this
