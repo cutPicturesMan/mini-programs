d54f994badd4b9e895af1cdf4440ecfe28b08352
your_name <your_email>
2018-04-12 23:36:10 +0800

    添加gulp

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const gutil = require('gulp-util')
+const autoprefixer = require('gulp-autoprefixer')
+const combiner = require('stream-combiner2')
+const rename = require("gulp-rename")
+
+let colors = gutil.colors;
+
+const handleError = function(err) {
+  console.log('\n')
+  gutil.log(colors.red('Error!'))
+  gutil.log('fileName: ' + colors.red(err.fileName))
+  gutil.log('lineNumber: ' + colors.red(err.lineNumber))
+  gutil.log('message: ' + err.message)
+  gutil.log('plugin: ' + colors.yellow(err.plugin))
+};
+
+gulp.task('sass', function () {
+  var combined = combiner.obj([
+    gulp.src('./pages/test/*.scss'),
+    sass().on('error', sass.logError),
+    autoprefixer([
+      'iOS >= 8',
+      'Android >= 4.1'
+    ]),
+    rename((path) => path.extname = '.wxss'),
+    gulp.dest('./pages/test/')
+  ]);
+
+  combined.on('error', handleError);
+});
+
+gulp.task('default', function () {
+  gulp.watch('./pages/**/*.scss', ['sass']);
+});
